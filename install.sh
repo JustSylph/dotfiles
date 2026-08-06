16af3aa1c46ac4086ddc81e8c9ab492dbf2c15f8
Sylph <sylph@tuta.com>
2026-08-07 00:18:32 +0200

    test

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PACKAGE_LIST="vim kitty gh i3 fastfetch feh curl vlc firefox fish"
+PACKAGE_LIST="vim kitty gh i3 fastfetch feh curl vlc fish"
 
 # install
 echo "Installing packages..."
-apt install -y $PACKAGE_LIST
+sudo apt install -y $PACKAGE_LIST
 echo "Packages installed."
 echo "Changing shell to fish..."
 chsh -s /usr/bin/fish
@@ -13,13 +13,10 @@ echo "Fisher installed."
 echo "Installing Tide..."
 fisher install IlanCosman/tide@v6
 echo "Tide installed."
-echo "Downloading dotfiles."
-git clone https://github.com/JustSylph/dotfiles
 echo "Configuring dotfiles..."
 mv ~/dotfiles/.bashrc ~
 mv ~/dotfiles/wallpaper ~
 mv ~/dotfiles/.config/fastfetch ~/.config/
 mv ~/dotfiles/.config/kitty ~/.config/
-feh --bg-tile ~/wallpaper/tiled.png
 rm -r ~/dotfiles
 echo "Installation finished!"
