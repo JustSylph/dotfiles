24bbb2558a5be3ef2ee7d55305f5e2e036ebda8d
Sylph <sylph@tuta.com>
2026-08-08 15:37:11 +0200

    test

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@ PACKAGE_LIST="filezilla vim kitty gh xorg fish i3 polybar fastfetch feh curl fir
 
 # symlinks
 mkdir ~/.config
-ln -s ~/.dotfiles/.config/* ~/.config/
+ln -s ~/.dotfiles/.config/fastfetch ~/.config/fastfetch
+ln -s ~/.dotfiles/.config/i3 ~/.config/i3
+ln -s ~/.dotfiles/.config/fish ~/.config/fish
+ln -s ~/.dotfiles/.config/kitty ~/.config/kitty
+ln -s ~/.dotfiles/.config/polybar ~/.config/polybar
 ln -s ~/.dotfiles/fonts/DepartureMono-1.500/DepartureMono-Regular.otf /usr/share/fonts/DepartureMono.otf
 ln -s ~/.dotfiles/.xinitrc ~/.xinitrc
 # packages
