cfc0c730d681b3665c897e188b45468961c511e2
Sylph <sylph@tuta.com>
2026-08-08 16:08:26 +0200

    fixed install.sh & .xinitrc

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 PACKAGE_LIST="filezilla vim kitty gh xorg fish i3 polybar fastfetch feh curl firefox-esr pipewire-audio blueman"
 
 # symlinks
-mkdir ~/.config
-ln -s ~/.dotfiles/.config/fastfetch ~/.config/fastfetch
-ln -s ~/.dotfiles/.config/i3 ~/.config/i3
-ln -s ~/.dotfiles/.config/fish ~/.config/fish
-ln -s ~/.dotfiles/.config/kitty ~/.config/kitty
-ln -s ~/.dotfiles/.config/polybar ~/.config/polybar
-sudo cp ~/.dotfiles/fonts/DepartureMono-1.500/DepartureMono-Regular.otf /usr/share/fonts/
-ln -s ~/.dotfiles/.xinitrc ~/.xinitrc
+mkdir /home/hydrogen/.config
+ln -s /home/hydrogen/.dotfiles/.config/fastfetch ~/.config/fastfetch
+ln -s /home/hydrogen/.dotfiles/.config/i3 ~/.config/i3
+ln -s /home/hydrogen/.dotfiles/.config/fish ~/.config/fish
+ln -s /home/hydrogen/.dotfiles/.config/kitty ~/.config/kitty
+ln -s /home/hydrogen/.dotfiles/.config/polybar ~/.config/polybar
+sudo cp /home/hydrogen/.dotfiles/fonts/DepartureMono-1.500/DepartureMono-Regular.otf /usr/share/fonts/
+ln -s /home/hydrogen/.dotfiles/.xinitrc ~/.xinitrc
 # packages
 sudo apt install -y $PACKAGE_LIST
 echo "Installation finished!"
