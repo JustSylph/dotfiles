32548b9632cdae339dc327f023b399636196018d
Sylph <sylph@tuta.com>
2026-08-08 15:19:02 +0200

    added .xinitrc, updated readme and install.sh

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-includes an install script
+Sylph Dotfiles
+==============
+
+Package List
+------------
+* filezilla
+* vim
+* kitty
+* gh
+* xorg
+* fish
+* i3
+* polybar
+* fastfetch
+* feh
+* curl
+* firefox-esr
+* pipewire-audio
+* blueman
+& dependencies
+
+install.sh
+----------
+* Installs above packages
+* Symlinks ~/.dotfiles/.config to ~/.config
+* Symlinks DepartureMono-Regular.otf to /usr/share/fonts/DepartureMono.otf
+* Changes shell to Fish
+
+.xinitrc
+--------
+* Sets tiled wallpaper
+* Runs blueman & pipewire-audio
+* Runs polybar
+* Starts i3
--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-PACKAGE_LIST="stow filezilla vim kitty gh xorg fish i3 polybar fastfetch feh curl firefox-esr pipewire-audio blueman"
+PACKAGE_LIST="filezilla vim kitty gh xorg fish i3 polybar fastfetch feh curl firefox-esr pipewire-audio blueman"
 
-# install
-echo "Installing packages..."
+# symlinks
+mkdir ~/.config
+ln -s ~/.dotfiles/.config/* ~/.config/
+ln -s ~/.dotfiles/fonts/DepartureMono-1.500/DepartureMono-Regular.otf /usr/share/fonts/DepartureMono.otf
+ln -s ~/.dotfiles/.xinitrc ~/.xinitrc
+# packages
 sudo apt install -y $PACKAGE_LIST
-echo "Packages installed."
-echo "Configuring dotfiles..."
-stow -d ~/dotfiles -t ~ -S .config
-echo "Installing fonts..."
-sudo mv ~/dotfiles/fonts/DepartureMono-1.500/DepartureMono-Regular.otf /usr/share/fonts
 echo "Installation finished!"
+# change shell to fish
+chsh -s /usr/bin/fish
