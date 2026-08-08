6bac3cb255d49a849d1562eb31cceeebd63d57e7
Sylph <sylph@tuta.com>
2026-08-08 13:43:24 +0200

    test i3 config, added a few packages

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-PACKAGE_LIST="vim kitty gh i3 polybar fastfetch feh curl firefox-esr pipewire-audio blueman"
+PACKAGE_LIST="stow filezilla vim kitty gh xorg fish i3 polybar fastfetch feh curl firefox-esr pipewire-audio blueman"
 
 # install
 echo "Installing packages..."
 sudo apt install -y $PACKAGE_LIST
 echo "Packages installed."
-#echo "Configuring dotfiles..."
-#mv ~/dotfiles/.bashrc ~
-#mv ~/dotfiles/wallpaper ~
-#mv ~/dotfiles/.config/fastfetch ~/.config/
-#mv ~/dotfiles/.config/kitty ~/.config/
-#rm -r ~/dotfiles
+echo "Configuring dotfiles..."
+stow -d ~/dotfiles -t ~ -S .config
+echo "Installing fonts..."
+sudo mv ~/dotfiles/fonts/DepartureMono-1.500/DepartureMono-Regular.otf /usr/share/fonts
 echo "Installation finished!"
